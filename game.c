bd542425b0d110bc365c4151c8f879bb42abb423
alb123 <albertfelix2005@gmail.com>
2025-09-13 12:28:08 -0700

    added checks for malloc

--- game.c
+++ game.c
@@ -26,7 +26,8 @@ GAME *createGame(int n)
         return NULL;
     } 
 
-   // creates a nxn array to store gamestate
+    // creates a nxn array to store gamestate
+    // cols init 
     rockfall->game_array = calloc(n, sizeof(CELL*));
     if(rockfall->game_array == NULL)
     {
@@ -34,29 +35,36 @@ GAME *createGame(int n)
         free(rockfall);
         return NULL;
     }
+
+    // rows init
     for(int i = 0; i < n; i++)
     {
        rockfall->game_array[i] = calloc(n, sizeof(CELL));
         if(rockfall->game_array[i] == NULL)
         {
             fprintf(stderr, "Error: gameArray row %d couldn't malloc\n", i);
-
+            for(int k = 0; k < i; k++) free(rockfall->game_array[k]); // prev rows
+            free(rockfall->game_array);
+            free(rockfall);
             return NULL;
         }
     }
 
+    // put player in middle
     rockfall->game_array[0][n/2] = CELL_PLAYER;
-
-    // store size and wincon for other threads
-    atomic_store(&rockfall->player_hit, 0);
-    rockfall->n = n;
-
 
     // setup mutex 
     if (pthread_mutex_init(&rockfall->mutex, NULL) != 0) {
         printf("Mutex init failed\n");
+        for(int i = 0; i < n; i++) free(rockfall->game_array[i]);
+        free(rockfall->game_array);
+        free(rockfall);
         return NULL;
     }
+
+    // store size and wincon for other threads
+    atomic_store(&rockfall->player_hit, 0);
+    rockfall->n = n;
 
     return rockfall;
 }
