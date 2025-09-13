7c9412a5ce44b92efba5a00c30c413a1f3322b18
alb123 <albertfelix2005@gmail.com>
2025-09-13 12:17:21 -0700

    working on init

--- game.c
+++ game.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <stdatomic.h>
+
+typedef struct {
+    CELL **game_array;
+    atomic_int player_hit;
+    int n;
+    pthread_mutex_t mutex;
+} GAME;
+
 
 typedef enum {
     CELL_EMPTY = 0b00,
@@ -8,14 +18,46 @@ typedef enum {
     CELL_PLAYER_HIT = 0b11
 } CELL;
 
-// creates a nxn array to store gamestate
-CELL **createGameArray(int n)
+GAME *createGame(int n)
 {
-    int **game_array = calloc(n, sizeof(CELL*));
+    GAME *rockfall = malloc(sizeof(GAME));
+    if(rockfall == NULL){
+        fprintf(stderr, "Error: game couldn't malloc\n");
+        return NULL;
+    } 
+
+   // creates a nxn array to store gamestate
+    rockfall->game_array = calloc(n, sizeof(CELL*));
+    if(rockfall->game_array == NULL)
+    {
+        fprintf(stderr, "Error: gameArray cols couldn't malloc\n");
+        free(rockfall);
+        return NULL;
+    }
     for(int i = 0; i < n; i++)
     {
-        game_array[i] = calloc(n, sizeof(CELL));
+       rockfall->game_array[i] = calloc(n, sizeof(CELL));
+        if(rockfall->game_array[i] == NULL)
+        {
+            fprintf(stderr, "Error: gameArray row %d couldn't malloc\n", i);
+
+            return NULL;
+        }
     }
-    game_array[0][n/2] = CELL_PLAYER;
+
+    rockfall->game_array[0][n/2] = CELL_PLAYER;
+
+    // store size and wincon for other threads
+    atomic_store(&rockfall->player_hit, 0);
+    rockfall->n = n;
+
+
+    // setup mutex 
+    if (pthread_mutex_init(&rockfall->mutex, NULL) != 0) {
+        printf("Mutex init failed\n");
+        return NULL;
+    }
+
+    return rockfall;
 }
 
